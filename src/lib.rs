754cc05ca38487fd4beb81b3335c906a516b4bd7
kud1ing <kud1ing@users.noreply.github.com>
2019-01-04 01:09:35 +0100

    clippy + fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 use std::iter::Peekable;
 use std::str::Chars;
 
-
 // TODO: Use `&str` or `Cow<'a, str>` instead of a `String`.
 #[derive(Clone, Debug, PartialEq)]
 enum Token {
@@ -14,9 +13,7 @@ enum Token {
 }
 
 impl Token {
-
     fn is_end_of_command(&self) -> bool {
-
         match self {
             Token::EndOfCommand => true,
             _ => false,
@@ -26,8 +23,7 @@ impl Token {
 
 ///
 #[derive(Debug, PartialEq)]
-enum EvaluationError {
-
+pub enum EvaluationError {
     ExpectedNumberOfArguments(String, u32),
     NoSuchCommand(String),
     NoSuchVariable(String),
@@ -39,12 +35,11 @@ type Evaluation = Result<String, EvaluationError>;
 
 // TODO: Use a `Cow<'a, str>` instead of a `String`.
 /// A function mapped to a command.
-type CommandFunction = fn(interpreter: &Interpreter, command_name: String, command_arguments: &[String]) -> Evaluation;
-
+type CommandFunction =
+    fn(interpreter: &Interpreter, command_name: &str, command_arguments: &[String]) -> Evaluation;
 
 /// The interpreter state.
-struct Interpreter {
-
+pub struct Interpreter {
     /// The commands known to the interpreter.
     commands: HashMap<&'static str, CommandFunction>,
 
@@ -52,12 +47,9 @@ struct Interpreter {
     variables: RefCell<HashMap<String, String>>,
 }
 
-
 impl Interpreter {
-
     /// Adds the given command.
     pub fn add_command(&mut self, command_name: &'static str, command_function: CommandFunction) {
-
         // TODO: Check that the given command is not yet registered.
 
         self.commands.insert(command_name, command_function);
@@ -65,28 +57,24 @@ impl Interpreter {
 
     /// Adds the standard commands.
     pub fn add_standard_commands(&mut self) {
-
         self.add_command(&"puts", command_puts);
         self.add_command(&"set", command_set);
     }
 
     /// Parses the given text to evaluate the contained commands.
     pub fn evaluate(&self, text: &str) -> Evaluation {
-
-        let mut command_parts : Vec<String> = Vec::new();
+        let mut command_parts: Vec<String> = Vec::new();
 
         // The result of the last command.
-        let mut last_result : Option<String> = None;
+        let mut last_result: Option<String> = None;
 
         // Create a peekable iterator over the characters of the given text.
         let character_iterator = &mut text.chars().peekable();
 
         // Iterate over the parsed tokens.
         while let Some(token) = next_token(character_iterator) {
-
             // The end of the current command is reached.
             if token.is_end_of_command() {
-
                 // Try to evaluate the command tokens.
                 last_result = Some(self.evaluate_command(&command_parts)?);
 
@@ -116,8 +104,7 @@ impl Interpreter {
     }
 
     /// Tries to evaluate the command with the given tokens.
-    fn evaluate_command(&self, command_parts: &Vec<String>) -> Evaluation {
-
+    fn evaluate_command(&self, command_parts: &[String]) -> Evaluation {
         // There are no command parts.
         if command_parts.is_empty() {
             return Ok("".to_owned());
@@ -131,31 +118,29 @@ impl Interpreter {
 
         // No such command is found.
         if maybe_command_function.is_none() {
-            return Err(EvaluationError::NoSuchCommand(command_name.to_string()))
+            return Err(EvaluationError::NoSuchCommand(command_name.to_string()));
         }
 
         let command_function = maybe_command_function.unwrap();
 
         // Call the command function with the command arguments.
-        command_function(self, command_name.to_string(), command_arguments)
+        command_function(self, command_name, command_arguments)
     }
-    
+
     /// Tries to evaluate the given token, which results in a string, if successful.
     fn evaluate_token(&self, token: Token) -> Evaluation {
-
         match token {
             Token::CommandSubstitution(string) => {
-
                 // Recursively evaluate the command.
                 self.evaluate(&string)
-            },
+            }
 
             Token::EndOfCommand => unimplemented!(),
 
             Token::String(string) => {
                 // Unwrap the string.
                 Ok(string)
-            },
+            }
 
             Token::VariableSubstitution(variable) => {
                 let maybe_value = self.get_variable(&variable);
@@ -165,7 +150,7 @@ impl Interpreter {
                 } else {
                     Ok(maybe_value.unwrap())
                 }
-            },
+            }
         }
     }
 
@@ -175,7 +160,6 @@ impl Interpreter {
 
     /// Instantiates an interpreter.
     pub fn new() -> Interpreter {
-
         Interpreter {
             commands: HashMap::new(),
             variables: RefCell::new(HashMap::new()),
@@ -191,8 +175,11 @@ fn character_is_end_of_command(character: char) -> bool {
     character == '\n' || character == ';'
 }
 
-fn command_puts(interpreter: &Interpreter, command_name: String, command_arguments: &[String]) -> Evaluation {
-
+pub fn command_puts(
+    _interpreter: &Interpreter,
+    _command_name: &str,
+    command_arguments: &[String],
+) -> Evaluation {
     let joined_arguments = command_arguments.join(" ").to_owned();
 
     println!("{}", joined_arguments);
@@ -200,14 +187,20 @@ fn command_puts(interpreter: &Interpreter, command_name: String, command_argumen
     Ok(joined_arguments)
 }
 
-fn command_set(interpreter: &Interpreter, command_name: String, command_arguments: &[String]) -> Evaluation {
-
+pub fn command_set(
+    interpreter: &Interpreter,
+    command_name: &str,
+    command_arguments: &[String],
+) -> Evaluation {
     if command_arguments.len() != 2 {
-        return Err(EvaluationError::ExpectedNumberOfArguments(command_name, 2));
+        return Err(EvaluationError::ExpectedNumberOfArguments(
+            command_name.to_owned(),
+            2,
+        ));
     }
 
-    let name = command_arguments.get(0).unwrap();
-    let value = command_arguments.get(1).unwrap();
+    let name = &command_arguments[0];
+    let value = &command_arguments[1];
 
     interpreter.set_variable(name.to_string(), value.to_string());
 
@@ -215,7 +208,6 @@ fn command_set(interpreter: &Interpreter, command_name: String, command_argument
 }
 
 fn consume_whitespaces(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
-
     // Try to peek at the next character.
     let next_character = *char_iterator.peek()?;
 
@@ -228,7 +220,6 @@ fn consume_whitespaces(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
 
     // Peek at the next character.
     while let Some(&next_character) = char_iterator.peek() {
-
         // The next character is a whitespace but not an end of command.
         if next_character.is_whitespace() && !character_is_end_of_command(next_character) {
             char_iterator.next();
@@ -241,7 +232,6 @@ fn consume_whitespaces(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
 }
 
 fn next_command_substitution_token(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
-
     // Try to peek at the next character.
     let next_character = *char_iterator.peek()?;
 
@@ -258,7 +248,6 @@ fn next_command_substitution_token(char_iterator: &mut Peekable<Chars>) -> Optio
 
     // Peek at the next character.
     while let Some(&next_character) = char_iterator.peek() {
-
         if next_character == '[' {
             char_iterator.next();
             command_nesting_level += 1;
@@ -266,8 +255,7 @@ fn next_command_substitution_token(char_iterator: &mut Peekable<Chars>) -> Optio
             if command_nesting_level > 1 {
                 string.push(next_character);
             }
-        }
-        else if next_character == ']' {
+        } else if next_character == ']' {
             char_iterator.next();
 
             if command_nesting_level > 1 {
@@ -293,10 +281,8 @@ fn next_command_substitution_token(char_iterator: &mut Peekable<Chars>) -> Optio
 }
 
 fn next_end_of_command_token(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
-
     // There is a next character.
     if let Some(&next_character) = char_iterator.peek() {
-
         // The next character is not an end of command.
         if !character_is_end_of_command(next_character) {
             return None;
@@ -316,14 +302,12 @@ fn next_end_of_command_token(char_iterator: &mut Peekable<Chars>) -> Option<Toke
 }
 
 fn next_string_token(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
-
     let string = parse_string(char_iterator)?;
 
     Some(Token::String(string))
 }
 
 fn next_token(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
-
     // The token parser functions.
     let token_parser_functions = &[
         consume_whitespaces,
@@ -335,7 +319,6 @@ fn next_token(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
 
     // Iterate over the token parser functions.
     for token_parser_function in token_parser_functions {
-
         // Use the current parser function.
         let maybe_token = token_parser_function(char_iterator);
 
@@ -349,7 +332,6 @@ fn next_token(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
 }
 
 fn next_variable_substitution_token(char_iterator: &mut Peekable<Chars>) -> Option<Token> {
-
     // Try to peek at the next character.
     let next_character = *char_iterator.peek()?;
 
@@ -366,7 +348,6 @@ fn next_variable_substitution_token(char_iterator: &mut Peekable<Chars>) -> Opti
 }
 
 fn parse_string(char_iterator: &mut Peekable<Chars>) -> Option<String> {
-
     // Try to peek at the next character.
     char_iterator.peek()?;
 
@@ -375,16 +356,10 @@ fn parse_string(char_iterator: &mut Peekable<Chars>) -> Option<String> {
 
     // Peek at the next character.
     while let Some(&next_character) = char_iterator.peek() {
-
-        // End of line.
-        if character_is_end_of_command(next_character) {
+        // End of line or whitespace.
+        if character_is_end_of_command(next_character) || next_character.is_whitespace() {
             break;
-        }
-        // Whitespace.
-        else if next_character.is_whitespace() {
-            break;
-        }
-        else {
+        } else {
             string.push(next_character);
             char_iterator.next();
         }
@@ -393,7 +368,6 @@ fn parse_string(char_iterator: &mut Peekable<Chars>) -> Option<String> {
     Some(string)
 }
 
-
 #[cfg(test)]
 mod tests {
 
@@ -401,7 +375,6 @@ mod tests {
 
     #[test]
     fn test_interpreter() {
-
         let mut interpreter = Interpreter::new();
         interpreter.add_standard_commands();
         interpreter.evaluate(&"puts Hello World");
@@ -413,9 +386,7 @@ mod tests {
 
     #[test]
     fn test_next_token() {
-
         let texts_and_expected_tokens = &[
-
             // Single tokens.
             ("", vec![Some(Token::EndOfCommand)]),
             (" ", vec![Some(Token::EndOfCommand)]),
@@ -424,23 +395,34 @@ mod tests {
             ("abc", vec![Some(Token::String("abc".to_owned()))]),
             ("\n", vec![Some(Token::EndOfCommand)]),
             (";", vec![Some(Token::EndOfCommand)]),
-            ("$blub", vec![Some(Token::VariableSubstitution("blub".to_owned()))]),
-
-            ("[command1 arg]", vec![Some(Token::CommandSubstitution("command1 arg".to_owned()))]),
-            ("[command2 arg1 arg2]", vec![Some(Token::CommandSubstitution("command2 arg1 arg2".to_owned()))]),
-            ("[a [b c]]", vec![Some(Token::CommandSubstitution("a [b c]".to_owned()))]),
-
+            (
+                "$blub",
+                vec![Some(Token::VariableSubstitution("blub".to_owned()))],
+            ),
+            (
+                "[command1 arg]",
+                vec![Some(Token::CommandSubstitution("command1 arg".to_owned()))],
+            ),
+            (
+                "[command2 arg1 arg2]",
+                vec![Some(Token::CommandSubstitution(
+                    "command2 arg1 arg2".to_owned(),
+                ))],
+            ),
+            (
+                "[a [b c]]",
+                vec![Some(Token::CommandSubstitution("a [b c]".to_owned()))],
+            ),
             // Unbalanced brackets.
             ("[a b", vec![None]),
             ("[a [b c]", vec![None]),
-
             // Combinations of tokens.
             (
                 "foo1 ",
                 vec![
                     Some(Token::String("foo1".to_owned())),
                     Some(Token::EndOfCommand),
-                ]
+                ],
             ),
             (
                 "foo2 bar   baz",
@@ -449,7 +431,7 @@ mod tests {
                     Some(Token::String("bar".to_owned())),
                     Some(Token::String("baz".to_owned())),
                     Some(Token::EndOfCommand),
-                ]
+                ],
             ),
             (
                 "foo3\nbar;baz",
@@ -460,7 +442,7 @@ mod tests {
                     Some(Token::EndOfCommand),
                     Some(Token::String("baz".to_owned())),
                     Some(Token::EndOfCommand),
-                ]
+                ],
             ),
             (
                 "puts [set a]",
@@ -468,13 +450,12 @@ mod tests {
                     Some(Token::String("puts".to_owned())),
                     Some(Token::CommandSubstitution("set a".to_owned())),
                     Some(Token::EndOfCommand),
-                ]
+                ],
             ),
         ];
 
         // Iterate over the texts and the expected tokens.
         for text_and_expected_tokens in texts_and_expected_tokens {
-
             let (text, expected_tokens) = text_and_expected_tokens;
 
             // println!("{:?}", text);
@@ -484,15 +465,11 @@ mod tests {
 
             // Iterate over the expected tokens.
             for expected_token in expected_tokens.iter().cloned() {
-
                 let token = next_token(&mut iterator);
 
                 // println!("  {:?}", token);
 
-                assert_eq!(
-                    token,
-                    expected_token
-                );
+                assert_eq!(token, expected_token);
             }
         }
     }
